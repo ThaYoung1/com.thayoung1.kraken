23c17bd2eb8fb04e0622c6159e008dd74acf3024
davydejong.ddj@gmail.com <33259655+ThaYoung1@users.noreply.github.com>
2023-02-22 17:56:15 +0100

    Update flow card explanation

--- app.js
+++ app.js
@@ -6,63 +6,81 @@ const Homey = require('homey');
 const KrakenClient = require('kraken-api');
 
 let assetPairs;
+let kraken;
 
 class KrakenApp extends Homey.App {
   async onInit() {
     this.log('KrakenApp has been initialized');
 
-    const key          = Homey.env.API_KEY;
-    const secret       = Homey.env.API_SECRET;
-    const kraken       = new KrakenClient(key, secret);
+    const key = Homey.env.API_KEY;
+    const secret = Homey.env.API_SECRET;
+    kraken = new KrakenClient(key, secret);
 
     assetPairs = await kraken.api('AssetPairs');
 
     const placeMarketOrderCard = this.homey.flow.getActionCard("place-market-order");
     placeMarketOrderCard.registerArgumentAutocompleteListener("pair", async (query, args) => { return this.autocompletePairs(query, args); });
     placeMarketOrderCard.registerArgumentAutocompleteListener("unit", async (query, args) => { return this.autocompleteUnits(query, args); });
+    placeMarketOrderCard.registerRunListener(async (args, state) => {
+      // the need to calculate order volume
+      var volume = await this.getVolume(args);
+      var result = await this.addOrder('market', volume, args);
+      throw new Error(JSON.stringify(result));
+    });
 
     const placeLimitOrderCard = this.homey.flow.getActionCard("place-limit-order");
     placeLimitOrderCard.registerArgumentAutocompleteListener("pair", async (query, args) => { return this.autocompletePairs(query, args); });
     placeLimitOrderCard.registerArgumentAutocompleteListener("unit", async (query, args) => { return this.autocompleteUnits(query, args); });
-
-    placeMarketOrderCard.registerRunListener(async (args, state) => {
+    placeLimitOrderCard.registerRunListener(async (args, state) => {
       // the need to calculate order volume
-      var volume = args.volume;
-      if (args.unit.baseorquote == args.pair.quote){
-        var ticker = await kraken.api('Ticker', { pair: args.pair.base + args.pair.quote });
-        var arrTicker = Object.values(ticker.result);
-
-        // 10 / +Object.values(ticker.result)[0].c[0] hier ben 
-        var volume = args.volume / arrTicker[0].c[0];
-        if (volume < args.pair.ordermin) {
-          throw new Error('Fout! Minimum niet gehaald');
-        }
-      }
-
-      var result = await kraken.api('AddOrder', { 
-        ordertype: 'market', // (req) market, limit, stop-loss, take-profit, stop-loss-limit, take-profit-limit, settle-position
-        type: args.type, // (req) buy, sell
-        volume: volume, // moet worden berekend met actuele prijs bij markt orders
-        pair: args.pair.base + args.pair.quote, // gettable?
-        validate: Homey.env.TESTMODE
-        //userref: 159753,
-        //expiretm: '1676565876' // expires in
-      });
-
+      var volume = await this.getVolume(args, args.price);
+      var result = await this.addOrder('limit', volume, args);
       throw new Error(JSON.stringify(result));
-      //return JSON.stringify(result);
-
     });
+
+    this.getOHLC = this.getOHLC.bind(this);
+    setInterval(this.getOHLC, Homey.env.OHLC_REFRESH_RATE * 1000);
+
+
   }
 
-  prettyPrintBaseQuote(item) {
-    if ( item.length >= 4 &&
-        (item.toUpperCase().startsWith('X') ||
-         item.toUpperCase().startsWith('Z')) ){
-        return item.substring(1);
-    } else {
-      return item;
+  async getOHLC(){
+    var result = await kraken.api('OHLC', { pair: 'XBTEUR', interval: 1440 });    
+    this.log(JSON.stringify(result));
+  }
+
+
+  async addOrder(ordertype, volume, args){
+    var result = await kraken.api('AddOrder', { 
+      ordertype: ordertype, // (req) market, limit, stop-loss, take-profit, stop-loss-limit, take-profit-limit, settle-position
+      type: args.type, // (req) buy, sell
+      volume: volume, // moet worden berekend met actuele prijs bij markt orders
+      pair: args.pair.base + args.pair.quote, // gettable?
+      validate: Homey.env.TESTMODE,
+      price: args.price
+      //userref: 159753,
+      //expiretm: '1676565876' // expires in
+    });
+
+    return result;
+  }
+
+  async getVolume(args, price = null){
+    var volume = args.volume;
+
+    if (args.unit.baseorquote == args.pair.quote){
+      if (!price){
+        var ticker = await kraken.api('Ticker', { pair: args.pair.base + args.pair.quote });
+        var arrTicker = Object.values(ticker.result);
+        price = arrTicker[0].c[0];
+      }
+
+      var volume = args.volume / price;
+      if (volume < args.pair.ordermin) {
+        throw new Error('Fout: Minimale ordergrootte van ' + args.pair.ordermin + ' niet gehaald');
+      }
     }
+    return volume;
   }
 
   autocompleteUnits(query, args){
@@ -99,6 +117,17 @@ class KrakenApp extends Homey.App {
        return x.name.toLowerCase().includes(query.toLowerCase());
      });
   }
+
+  prettyPrintBaseQuote(item) {
+    if ( item.length >= 4 &&
+        (item.toUpperCase().startsWith('X') ||
+         item.toUpperCase().startsWith('Z')) ){
+        return item.substring(1);
+    } else {
+      return item;
+    }
+  }
+
 
 }
 
